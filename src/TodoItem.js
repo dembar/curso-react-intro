5cb361ef0af188c1e75e695f0ab7cc47dd60b3e5
Danny MB <danielmasias4@gmail.com>
2024-02-01 17:39:46 -0500

    nothing to comment

---
+++ src/TodoItem.js
@@ -0,0 +1,20 @@
+import './TodoItem.css'
+import { AiFillAlert } from "react-icons/ai";
+
+function TodoItem(props){
+    return (
+      <li className = "TodoItem">
+        {/* <span 
+          onClick={props.onComplete}
+          className = {`Icon Icon-check ${props.completed && "Icon-check--active"}`}></span> */}
+        
+        <AiFillAlert />
+        <p className = {`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
+        <span 
+          onClick={props.onDelete}
+          className = "Icon Icon-delete">X</span>
+      </li>
+    );
+  }
+
+  export {TodoItem}
